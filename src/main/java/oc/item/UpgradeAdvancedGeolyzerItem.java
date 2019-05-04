eb8b08e94866d750013edde5d4d9f1cfe038a9d3
grand8master <java9compiler@gmail.com>
2019-05-04 22:17:21 +0300

    add lore for the geolyzer upgrade

--- src/main/java/oc/item/UpgradeAdvancedGeolyzerItem.java
+++ src/main/java/oc/item/UpgradeAdvancedGeolyzerItem.java
@@ -7,12 +7,16 @@ import li.cil.oc.api.driver.item.Slot;
 import li.cil.oc.api.internal.Robot;
 import li.cil.oc.api.network.EnvironmentHost;
 import li.cil.oc.api.network.ManagedEnvironment;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.StatCollector;
 import oc.Config;
 import oc.Omega;
 import oc.driver.DriverUpgradeAdvancedGeolyzer;
+
+import java.util.List;
 
 public class UpgradeAdvancedGeolyzerItem extends Item implements li.cil.oc.api.driver.Item, EnvironmentProvider, HostAware {
 
@@ -20,6 +24,13 @@ public class UpgradeAdvancedGeolyzerItem extends Item implements li.cil.oc.api.d
         setCreativeTab(CreativeTab.instance);
         setUnlocalizedName("upgrade_advanced_geolyzer");
         setTextureName(Omega.MOD_ID + ":upgrade_advanced_geolyzer");
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean p_77624_4_) {
+        list.add(StatCollector.translateToLocal("lore.geolyzer_tier.name") + " " + Config.GEOLYZER_TIER);
+        list.add(StatCollector.translateToLocal("lore.geolyzer_scan_cost.name") + " " + Config.GEOLYZER_SCAN_COST);
+        list.add(StatCollector.translateToLocal("lore.geolyzer_radius.name") + " " + Config.GEOLYZER_RADIUS);
     }
 
     @Override
